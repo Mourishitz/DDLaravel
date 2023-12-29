12361782b928e6214bf30dfb68ba00f9dee6cc1f
Mourishitz <gabrielmorishita@hotmail.com>
2023-12-29 00:31:06 -0300

    wipper

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,20 +7,17 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     */
+    protected $commands = [
+        \App\Console\Commands\MakeModule::class,
+    ];
+
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
     }
 
-    /**
-     * Register the commands for the application.
-     */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
